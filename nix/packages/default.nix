63e1a0232ee34d9e03f0a904ae2bbeaa83651c48
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2025-04-25 15:04:41 +0200

    feat: bring back op-deployer with gb fixes

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -13,9 +13,10 @@
       contracts-bedrock-v1_7_0-beta_1_l2-contracts =
         callPackage ./contracts-bedrock/v1.7.0-beta.1+l2-contracts
         {};
-      op-deployer-v0_2_0-rc2 = callPackage ./op-deployer/op-deployer-v0.2.0-rc.2.nix {};
 
       op-deployer = op-deployer-v0_2_0-rc2;
+      op-deployer-v0_2_0-rc2 = callPackage ./op-deployer/op-deployer-v0.2.0-rc.2.nix {};
+      op-deployer-gb = callPackage ./op-deployer/op-deployer-gb.nix { };
 
       init-rollup-config = callPackage ./init-rollup-config.nix {inherit op-deployer;};
     };
