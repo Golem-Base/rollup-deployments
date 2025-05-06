cbe605b57c0708fb9d12b3194352669cf3b25cd5
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2025-05-06 15:07:20 +0100

    added simple script for multi rpc

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -3,7 +3,44 @@ _: {
     pkgs,
     self',
     ...
-  }: {
+  }: let
+    el-rpc = pkgs.writeShellScriptBin "el-rpc" ''
+      # Define the RPC command and jq filter from arguments
+      rpc_cmd=$1
+      jq_filter=$2
+      nodes="PYX ZAS ELK KEX AVO URB"
+      temp_file=$(mktemp)
+      for node in $nodes; do
+        (
+          node_var="''${node}_EXECUTION_RPC"
+          rpc_url=$(printenv "$node_var")
+          result=$(cast rpc $rpc_cmd --rpc-url "$rpc_url" --json | jq "$jq_filter" 2>/dev/null || echo "ERROR")
+          echo "$node $result" >> "$temp_file"
+        ) &
+      done
+      wait
+      sort "$temp_file" | column -t
+      rm "$temp_file"
+    '';
+
+    cl-rpc = pkgs.writeShellScriptBin "cl-rpc" ''
+      rpc_cmd=$1
+      jq_filter=$2
+      nodes="PYX ZAS ELK KEX AVO URB"
+      temp_file=$(mktemp)
+      for node in $nodes; do
+        (
+          node_var="''${node}_CONSENSUS_RPC"
+          rpc_url=$(printenv "$node_var")
+          result=$(cast rpc $rpc_cmd --rpc-url "$rpc_url" --json | jq "$jq_filter" 2>/dev/null || echo "ERROR")
+          echo "$node $result" >> "$temp_file"
+        ) &
+      done
+      wait
+      sort "$temp_file" | column -t
+      rm "$temp_file"
+    '';
+  in {
     devshells.default = {
       packages = with pkgs; [
         age
@@ -19,6 +56,8 @@ _: {
         doppler
 
         docker
+        el-rpc
+        cl-rpc
 
         self'.packages.gb-deployer
         self'.packages.op-deployer
