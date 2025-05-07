2961871c636175e380f38aa9d72984dab9164af4
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2025-05-07 12:52:08 +0100

    more dockerfile fixings

--- docker/scripts/init.sh
+++ docker/scripts/init.sh
@@ -24,6 +24,7 @@ openssl rand -hex 32 > /init/jwt
 
 if [ -n "${POD_NAME:-}" ]; then
   POD_BASE_NAME=$(echo "${POD_NAME}" | sed -E 's/-[0-9]+$//')
+
   if [ -n "${POD_BASE_NAME:-}" ]; then
     IP=$(getent hosts "$POD_BASE_NAME.$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace).svc.cluster.local" | awk '{ print $1 }')
 
@@ -32,33 +33,27 @@ if [ -n "${POD_NAME:-}" ]; then
       echo "Found service for pod at ip: $(cat /init/service_ip)"
     else
       echo "Failed to find IP from POD_BASE_NAME: $POD_BASE_NAME"
+      exit 1
     fi
-  else
-    echo "Failed to extract POD_BASE_NAME from POD_NAME: $POD_NAME"
-  fi
-else
-  echo "POD_NAME variable wasn't set, skipping service IP extraction"
-fi
 
+    PEERS_JSON_PATH="/peers/peers.json"
+    if [ -f "$PEERS_JSON_PATH" ]; then
+      PEERS_JSON=$(jq -r --arg pod_base_name "$POD_BASE_NAME" '.[] | select(.name != $pod_base_name)' $PEERS_JSON_PATH)
+      echo -e "Found peers: \n$PEERS_JSON\n"
 
-PEERS_JSON_PATH="/peers/peers.json"
-if [[ -f "$PEERS_JSON_PATH" ]]; then
-  PEERS_JSON=$(jq -r --arg pod_base_name "$POD_BASE_NAME" '.[] | select(.name != $pod_base_name)' $PEERS_JSON_PATH)
-  echo -e "Found peers: \n$PEERS_JSON\n"
+      echo "$PEERS_JSON" \
+        | jq -r ".consensus" \
+        | tr '\n' ',' \
+        | sed 's/,$//' \
+        > /init/op_node_static_peers
 
-  echo "$PEERS_JSON" \
-    | jq -r ".consensus" \
-    | tr '\n' ',' \
-    | sed 's/,$//' \
-    > /init/op_node_static_peers
+      EXECUTION_PEERS=$(echo "$PEERS_JSON" \
+        | jq -r ".execution" \
+        | sed 's/^/  "/' \
+        | sed 's/$/",/' \
+        | sed '$s/,$//')
 
-  EXECUTION_PEERS=$(echo "$PEERS_JSON" \
-    | jq -r ".execution" \
-    | sed 's/^/  "/' \
-    | sed 's/$/",/' \
-    | sed '$s/,$//')
-
-  cat > /init/geth_config << EOF
+      cat > /init/geth_config << EOF
 [Node.P2P]
 StaticNodes = [
 $EXECUTION_PEERS
@@ -67,6 +62,14 @@ TrustedNodes = [
 $EXECUTION_PEERS
 ]
 EOF
+    else
+      echo "$PEERS_JSON_PATH does not exist, skipping op-node and op-geth static peer construction" 
+      exit 1
+    fi
+  else
+    echo "Failed to extract POD_BASE_NAME from POD_NAME: $POD_NAME"
+    exit 1
+  fi
 else
-  echo "$PEERS_JSON_PATH does not exist, skipping op-node and op-geth static peer construction" 
+  echo "POD_NAME variable wasn't set, skipping service IP extraction and peering setup"
 fi
